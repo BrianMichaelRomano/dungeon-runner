df3656e1f8799584af73da710840cdccffbee19b
Brian Michael Romano <brianmromano@gmail.com>
2018-02-24 11:53:29 -0500

    latest webpack  build

--- app/build/bundle.js
+++ app/build/bundle.js
@@ -78,6 +78,7 @@ const view = new ViewController();
 // logic
     // Render Player Attributes to  view
 view.renderAttributes(player);
+view.renderStatistics(player.getCalculatedStats());
 
 /***/ }),
 /* 1 */
@@ -85,12 +86,48 @@ view.renderAttributes(player);
 
 module.exports =  class PlayerModel {
     constructor(name) {
+        // Attributes
+        // CON: Constitution
+        // STR: Stregth
+        // DEX: Dexterity
+        // INT: Intelligence
+        // WIS: Wisdom
+
         this.name = name;
         this.CON = 10;
         this.STR = 10;
         this.DEX = 10;
         this.INT = 10;
         this.WIS = 10;
+    }
+
+    getCalculatedStats() {
+        // Statistics
+        // HP: Hit Points - CON, STR
+        // AP: Action Points - DEX, CON
+        // MP: Magic Points - INT
+        // AR: Armor Rating - Based on equipment
+        // DR: Defense Rating - CON, DEX
+        // AC: Attack Chance - DEX, STR
+        // MC: Magic Chance - WIS
+        // WER: Weapon Effect Rating - Weapon, STR
+        // MER: Magic Effect Rating - INT, WIS
+        // IR: Initiative Rating - DEX
+
+        const playerStats = {
+            HP: 10 + this.CON + this.STR,
+            AP: 10 + this.DEX + this.CON,
+            MP: this.INT * 10,
+            AR: null,
+            DR: this.CON + this.DEX,
+            AC: this.DEX + this.STR,
+            MC: this.WIS,
+            WER: null,
+            MER: this.INT + this.WIS,
+            IR: this.DEX,
+        };
+
+        return playerStats;
     }
 }
 
@@ -103,6 +140,7 @@ module.exports = class ViewController {
     constructor() {
         // select view elements
         this.charAttributeList = document.querySelector('#charAttributeList');
+        this.charStatisticsList = document.querySelector('#charStatisticsList');
     }
     // Render Player Attributes In <ul>
     renderAttributes(player) {
@@ -118,6 +156,27 @@ module.exports = class ViewController {
         console.log('View Rendered Player: ',player);
         // set <ul> innerHTML to template output
         this.charAttributeList.innerHTML = output;
+    }
+
+    // Render Player Statistics in <ul>
+    renderStatistics(playerStats) {
+        let output = `
+        <li>Hit Points: ${playerStats.HP}</li>
+        <li>Action Points: ${playerStats.AP}</li>
+        <li>Magic Points: ${playerStats.MP}</li>
+        <li>Armor Rating: ${playerStats.AR}</li>
+        <li>Defense Rating: ${playerStats.DR}</li>
+        <li>Attack Chance: ${playerStats.AC}</li>
+        <li>Magic Chance: ${playerStats.MC}</li>
+        <li>Weapon Effect Rating: ${playerStats.WER}</li>
+        <li>Magic Effect Rating: ${playerStats.MER}</li>
+        <li>Initiative: ${playerStats.IR}</li>
+    `;
+
+    // Log player Statistics used in template 
+    console.log('View Rendered Player: ',playerStats);
+    // set <ul> innerHTML to template output
+    this.charStatisticsList.innerHTML = output;
     }
 }
 
