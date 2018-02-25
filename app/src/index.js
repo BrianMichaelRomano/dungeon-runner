4a07200b87ba009fd8c6b8662c877424cf9acd60
Brian Michael Romano <brianmromano@gmail.com>
2018-02-25 12:06:59 -0500

    loads current view after browser refresh

--- app/src/index.js
+++ app/src/index.js
@@ -2,3 +2,5 @@
 const viewRouteController = require('./modules/view/viewRouteController');
 // Register event listeners for routes
 viewRouteController.registerListeners();
+// Load view saved in state if exists or load home view
+viewRouteController.loadCurrentView();
