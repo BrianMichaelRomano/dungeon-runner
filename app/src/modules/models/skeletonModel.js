bc3e1f431d060c131fd72ec2c575b625ba2b67dc
Brian Michael Romano <brianmromano@gmail.com>
2018-02-24 20:48:55 -0500

    add skeleton model

---
+++ app/src/modules/models/skeletonModel.js
@@ -0,0 +1,17 @@
+module.exports =  class SkeletonModel {
+    constructor() {
+        // Attributes
+        // CON: Constitution
+        // STR: Stregth
+        // DEX: Dexterity
+        // INT: Intelligence
+        // WIS: Wisdom
+
+        this.name = "Skeleton";
+        this.CON = 10;
+        this.STR = 10;
+        this.DEX = 10;
+        this.INT = 10;
+        this.WIS = 10;
+    }
+}
