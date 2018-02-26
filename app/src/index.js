c6739d7105ccd6db4f1132bcff135a0754a31ae3
Brian Michael Romano <brianmromano@gmail.com>
2018-02-25 21:05:51 -0500

    comment update

--- app/src/index.js
+++ app/src/index.js
@@ -2,5 +2,5 @@
 const viewRouteController = require('./modules/view/viewRouteController');
 // Register event listeners for routes
 viewRouteController.registerListeners();
-// Load view saved in state if exists or load home view
+// Load view saved in state if exists or load home view on browser refresh
 viewRouteController.loadCurrentView();
