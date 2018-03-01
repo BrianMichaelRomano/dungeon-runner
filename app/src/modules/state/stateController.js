523cedce36477bed9ecf7ab92109f1701d57dba0
Brian Michael Romano <brianmromano@gmail.com>
2018-02-28 19:45:10 -0500

    refactored so view and and entity states are thier own objects

--- app/src/modules/state/stateController.js
+++ app/src/modules/state/stateController.js
@@ -1,10 +1,32 @@
 module.exports = {
-    // Set view state
-    setViewState(view) {
-        localStorage.setItem('view', JSON.stringify(view));
+    view: {
+        // Set view state
+        setViewState(view) {
+            localStorage.setItem('view', JSON.stringify(view));
+        },
+        // Get view state
+        getViewState() {
+            return JSON.parse(localStorage.getItem('view'));
+        }
     },
-    // Get view state
-    getViewState() {
-        return JSON.parse(localStorage.getItem('view'));
+    entity: {
+        getPlayer() {
+            const player = {
+                name: 'Firecore',
+                HP: 100,
+                AP: 80,
+                MP: 60
+            }
+            return player;
+        },
+        getSkeleton() {
+            const skeleton = {
+                name: 'Skeleton',
+                HP: 80,
+                AP: 60,
+                MP: 40
+            }
+            return skeleton;
+        }
     }
 }
