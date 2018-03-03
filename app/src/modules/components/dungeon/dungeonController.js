61d68d1e4a475c64cd6b0efc75fc930a62259b5a
Brian Michael Romano <brianmromano@gmail.com>
2018-03-03 15:25:20 -0500

    cleanup

--- app/src/modules/components/dungeon/dungeonController.js
+++ app/src/modules/components/dungeon/dungeonController.js
@@ -12,10 +12,8 @@ module.exports = {
 
         // Render dungeon entrance
         renderEntrance: function() {
-
             // Checks if dungeon has been entered
             if(storageState.dungeon.getDungeonState().status === 'fresh') {
-
                 // Render entrance view
                 _.element('#entrance').innerHTML = dungeonViews.entrance();
 
@@ -31,26 +29,23 @@ module.exports = {
                 this.renderDungeon();
             }
         },
+
         // Renders dungeon view
         renderDungeon: function() {
-            
             // Get all states in storage
             const varStates = storageState.getAllStates();
-            
             // this is where dungeon messages should be displayed
             _.element('#dungeon-messages').innerHTML = dungeonViews.dungeonMessages('Running dungeon!');
-            
             // Template for entity card views
             _.element('#entity-cards').innerHTML = dungeonViews.entityCards(varStates);
-            
             // Template for action buttons
             _.element('#action-btns').innerHTML = dungeonViews.actionButtons();
-            
             // Action Button Listeners
             _.element('#attack-btn').addEventListener('click', () => {
                 this.attackBtnPressed();
             });
         },
+
         // Attack Button Pressed
         attackBtnPressed : function() {
             const varStates = storageState.getAllStates();
