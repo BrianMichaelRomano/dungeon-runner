e17d52b629e4737202cc758b8b01965db2c69031
Brian Michael Romano <brianmromano@gmail.com>
2018-03-04 11:42:10 -0500

    comments

--- app/src/modules/model.js
+++ app/src/modules/model.js
@@ -1,11 +1,16 @@
+// Set current view to Local Storage
 const setCurrentView = function(activeView) {
     localStorage.setItem('view', JSON.stringify(activeView));
 }
 
+
+// Get current view saved in Local Storage
 const getCurrentView = function() {
+    // Check for saved value in local storage and return it
     if(localStorage.getItem('view')) {
         return JSON.parse(localStorage.getItem('view'));
     } else {
+        // else return home view
         return 'home-view';
     }
 }
