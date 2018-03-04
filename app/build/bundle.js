91d7fcde5147486c2d14be506ac15187b67989e8
Brian Michael Romano <brianmromano@gmail.com>
2018-03-03 20:53:53 -0500

    add basic attack button listener

--- app/build/bundle.js
+++ app/build/bundle.js
@@ -548,26 +548,30 @@ module.exports = {
         switch (buttonPressed) {
             case 'attack-btn':
                 _.element('#action-menu').innerHTML = actionButtonsView.attackView();
+                _.element('#basic-attack-btn').addEventListener('click', () => {
+                    console.log('Basic Attack....');
+                });
                 console.log('Attack Menu');
                 break;
+
                 case 'defend-btn':
                 _.element('#action-menu').innerHTML = actionButtonsView.defendView();                
                 console.log('Defend Menu');
                 break;
+
                 case 'magic-btn':
                 _.element('#action-menu').innerHTML = actionButtonsView.magicView();                
                 console.log('Magic Menu');
                 break;
+
                 case 'item-btn':
                 _.element('#action-menu').innerHTML = actionButtonsView.itemView();                
                 console.log('Item Menu');
                 break;
+
                 case 'flee-btn':
                 _.element('#action-menu').innerHTML = actionButtonsView.fleeView();                
                 console.log('Flee Menu');
-                break;
-        
-            default:
                 break;
         }
     }
