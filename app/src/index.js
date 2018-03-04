e3419a0b2772ae6c7cd6ff3ac1e2d06597ec1b1a
Brian Michael Romano <brianmromano@gmail.com>
2018-03-03 22:03:18 -0500

    Revert "cleanup"
    This reverts commit d83a81c542dafda682b645759d9b28e0edac018a.

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,9 @@
 // Load Route Controller
 const viewRouteController = require('./modules/view/viewRouteController');
-
 // Load State Controller
 const state = require('./modules/state/stateController');
-
 // Register event listeners for routes
 viewRouteController.registerListeners();
-
 // Load view saved in state if exists or load home view on browser refresh
 viewRouteController.loadCurrentView();
 
