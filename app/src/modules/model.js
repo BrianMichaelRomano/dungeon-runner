a3991f00b34e2eb158dcd00678d6757d010c3cc8
Brian Michael Romano <brianmromano@gmail.com>
2018-03-04 11:03:26 -0500

    add getcurrentView method

--- app/src/modules/model.js
+++ app/src/modules/model.js
@@ -1,7 +1,12 @@
-const saveActiveViewModel = function(activeView) {
+const setCurrentView = function(activeView) {
     console.log(`${activeView} saved...`);
 }
 
+const getCurrentView = function() {
+    console.log(`Getting View model...`);
+}
+
 export {
-    saveActiveViewModel
+    setCurrentView,
+    getCurrentView
 }
