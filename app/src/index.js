329f426af9c3c35101cb3ed82c62061d84530a8b
Brian Michael Romano <brianmromano@gmail.com>
2018-03-04 11:03:59 -0500

    update

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,27 @@
-import { loadNavListener } from './modules/view.js';
-import { saveActiveViewModel } from './modules/model.js';
+// View Module
+import { loadNavListener, loadCurrentView } from './modules/view.js';
+// Model Module
+import { setCurrentView, getCurrentView } from './modules/model.js';
+
+// View Init
+const viewInit = function() {
+    const currentRoute = getCurrentView();
+    console.log('Init view...')
+}
+
+// View Selected
+const viewSelected = function(activeView) {
+    setCurrentView(activeView);
+}
+
+// =============================================================================
 
 // Load Navigation listener
 loadNavListener();
 
-// View Selected
-const viewSelected = function(activeView) {
-    saveActiveViewModel(activeView);
-}
+// Load current view
+viewInit();
+
 
 export {
     viewSelected
