bd6022b996b50da200414f285e3f36ef873e3c80
Brian Michael Romano <brianmromano@gmail.com>
2018-03-09 05:59:16 -0500

    comments

--- app/src/modules/state.js
+++ app/src/modules/state.js
@@ -1,31 +1,48 @@
-const newState = {
+// Default State Object
+const defaultState = {
+    // Last Saved Route
     route: 'home'
 }
 
+// Accepts a route and saves it to state
 function setRoute(route) {
+    // get current state
     const state = getState();
+    // set recived route to state object
     state.route = route;
+    // save updated state
     setState(state);
 }
 
+// Gets last saved route
 function getRoute() {
+    // get current state
     const state = getState();
+    // return route from state
     return state.route;
 }
 
+// Gets current state or sets and returns default state
 function getState() {
+    // if current state does not exists 
     if(!localStorage.getItem('DRstate')) {
-        setState(newState);
+        // set default state
+        setState(defaultState);
+        // return state
         return getState();
     } else {
+        // return state
         return JSON.parse(localStorage.getItem('DRstate'));
     }
 }
 
+// Accepts state object and sets to localStorage 
 function setState(state) {
+    // set state to localStorage
     localStorage.setItem('DRstate', JSON.stringify(state));
 }
 
+// Exports
 export {
     setRoute,
     getRoute
