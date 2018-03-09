bf05a8f55feb2f48b52d7d3b3adda4b83d89cdaf
Brian Michael Romano <brianmromano@gmail.com>
2018-03-08 20:59:34 -0500

    add enemy card

--- app/src/components/dungeon-component/dungeon-component.js
+++ app/src/components/dungeon-component/dungeon-component.js
@@ -1,6 +1,7 @@
 function dungeonRender() {
     console.log('Initialize Dungeon Component...');
     document.querySelector('#player-card').innerHTML = 'Firecore';
+    document.querySelector('#enemy-card').innerHTML = 'Skeleton';
 }
 
 export { 
