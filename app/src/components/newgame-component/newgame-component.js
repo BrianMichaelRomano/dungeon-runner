60811a57aca4e41fefd156fe1a43a87f79a85988
Brian Michael Romano <brianmromano@gmail.com>
2018-03-10 20:10:08 -0500

    add creatting new game

--- app/src/components/newgame-component/newgame-component.js
+++ app/src/components/newgame-component/newgame-component.js
@@ -24,10 +24,19 @@ class NewgameComponent {
         }
 
         // select input and buttons
+        const charNameInput = document.getElementById('character-name-input');
+        const charNameSubmit = document.getElementById('character-name-submit');
 
         // add listener on button to save entered name to state
+        charNameSubmit.addEventListener('click', () => {
+            // Set submitted name to state
+            let stateUpdate = state.createState();
+            stateUpdate.character.name = charNameInput.value;
+            state.setState(stateUpdate);
+            // render Inn Component on submit of character name
+            InnComponent.render('page');
+        });
 
-        // render Inn Component on submit of character name
     }
 }
 
