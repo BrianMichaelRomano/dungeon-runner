f6af8d1d3c0c7bd1f3db0f3c20e62d8c44c475b5
Brian Michael Romano <brianmromano@gmail.com>
2018-03-14 20:45:03 -0400

    revert to original engine from blog and modified to use {{}} instead of <% %>

--- app/src/services/templateEngine.js
+++ app/src/services/templateEngine.js
@@ -1,21 +1,9 @@
 // This wizardry was found here
-//https://github.com/krasimir/absurd/blob/master/lib/processors/html/helpers/TemplateEngine.js
+// http://krasimirtsonev.com/blog/article/Javascript-template-engine-in-just-20-line
 
 export class TemplateEngine {
     static renderTemplate(html, options) {
-        // Original reExp line. Modified so that i can use {{ property }} syntax in template. 
-        // This may break scripts in you templates. This is not needed for my purposes.
-        // var re = /<%(.+?)%>/g, <---- Replace following line with this to restore
-        var re = /{{(.+?)}}/g, 
-            
-            // Original reExp line. Modified so that i can use {{ property }} syntax in template. 
-            // This may break scripts in you templates. This is not needed for my purposes.
-            // reExp = /(^( )?(var|if|for|else|switch|case|break|{|}|;))(.*)?/g, <---- Replace following line with this to restore
-            reExp = /(^( )?(var|if|for|else|switch|case|break|;))(.*)?/g, 
-            code = 'with(obj) { var r=[];\n', 
-            cursor = 0, 
-            result,
-                match;
+        var re = /{{([^%>]+)?}}/g, reExp = /(^( )?(if|for|else|switch|case|break))(.*)?/g, code = 'var r=[];\n', cursor = 0, match;
         var add = function(line, js) {
             js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
                 (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
@@ -26,9 +14,7 @@ export class TemplateEngine {
             cursor = match.index + match[0].length;
         }
         add(html.substr(cursor, html.length - cursor));
-        code = (code + 'return r.join(""); }').replace(/[\r\t\n]/g, ' ');
-        try { result = new Function('obj', code).apply(options, [options]); }
-        catch(err) { console.error("'" + err.message + "'", " in \n\nCode:\n", code, "\n"); }
-        return result;
+        code += 'return r.join("");';
+        return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
     }
 }
