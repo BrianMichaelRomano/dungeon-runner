d3d062e877ce7cf28050fa698d863b6de88dcadd
Brian Michael Romano <brianmromano@gmail.com>
2018-03-16 22:01:28 -0400

    skeleton model

---
+++ app/src/entities/skeleton.js
@@ -0,0 +1,10 @@
+export class Skeleton {
+    constructor(name) {
+        this.name = 'Skeleton',
+        this.HP = 80,
+        this.AP = 60,
+        this.MP = 40,
+        this.armor = 2,
+        this.attack = 5
+    }
+}
